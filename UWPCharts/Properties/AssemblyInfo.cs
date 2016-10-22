fc737eaff5f28f2d1c9bd382b0f1030d1da4d95e
ailon <ailon@ailon.org>
2016-10-22 14:15:58 +0300

    updated assembly information

--- UWPCharts/Properties/AssemblyInfo.cs
+++ UWPCharts/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWPCharts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UWP QuickCharts")]
+[assembly: AssemblyDescription("Simple charting library for UWP apps")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UWPCharts")]
+[assembly: AssemblyCompany("Alan Mendelevich")]
+[assembly: AssemblyProduct("UWP QuickCharts")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
